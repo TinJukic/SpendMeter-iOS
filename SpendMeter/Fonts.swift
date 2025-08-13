3939357f3b648daa540b14d83e67ef8b59fc272e
Tin Jukić <tin.jukic.sb@gmail.com>
2025-08-13 22:17:07 +0200

    Added fonts

---
+++ SpendMeter/Fonts.swift
@@ -0,0 +1,17 @@
+//
+//  Fonts.swift
+//  SpendMeter
+//
+//  Created by Tin on 13.08.2025..
+//
+
+import SwiftUI
+
+public extension Font {
+    static let title: Font = .system(size: 24, weight: .semibold)
+    static let sectionTitle: Font = .system(size: 18, weight: .medium)
+    static let body: Font = .system(size: 16, weight: .regular)
+    static let secondaryText: Font = .system(size: 14, weight: .regular)
+    static let button: Font = .system(size: 16, weight: .semibold)
+    static let balance: Font = .system(size: 28, weight: .bold)
+}
